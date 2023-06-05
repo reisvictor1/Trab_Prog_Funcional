a525f118209fd8797a39440032b17c9252905ee7
reisvictor1 <reisvictor448@gmail.com>
2023-06-05 10:15:37 -0300

    function passes 2 returns

--- trab.hs
+++ trab.hs
@@ -8,35 +8,37 @@ getUserInputs = do
     let ls = words inputLine
     let intList = map read ls :: [Int]
     return intList
+{- 
 
-{-
-printScore[a:b:c:t] = do
-    let r_a = show a
-    let r_b = show b
+printScore[a:b:c:t] = 
     if(a==10) then print "X _|"
-    else if(a+b == 10) then print r_a $ print "/|"
-    else print r_a $ print r_b
+    else if(a+b == 10) then print $ show a ++ "/|"
+    else print $ show a ++ "" ++ show b
 -}
 
-score :: Int -> [Int] -> Int
-score sumList [] = sumList
-score sumList [a] = a + sumList
-score sumList [a, b] = sumList + a + b
-score sumList [a, b, c] = a + b + c + sumList
-score sumList (a:b:c:t)
-  | a == 10 = sumList + a + b + c + score sumList (b:c:t)
-  | a+b == 10 = sumList + a + b + c + score sumList (c:t)
-  | otherwise = sumList + a + b + score sumList (c:t)
+score :: Int -> [Int] -> (IO (), Int)
+score sumList [] = (print "", sumList)
+score sumList [a] = (print "", a + sumList)
+score sumList [a, b] = (print "", sumList + a + b)
+score sumList [a, b, c] = (print "", a + b + c + sumList)
+score sumList (a:b:c:t) =
+    if(a == 10) then
+        (print "", sumList + a + b + c + score sumList  (b:c:t))   
+    else if(a+b == 10) then
+        (print "", sumList + a + b + c + score sumList (c:t))
+        
+    else 
+        (print "", sumList + a + b + score sumList (c:t))
 
 
-partida :: [Int] -> Int
+partida :: IO [Int] -> IO Int
 partida l = do
-    let sumList = 0
-    let finalScore = score sumList l
-    print finalScore
+    let sumList = 0 
+    list <- l
+    let finalScore = score sumList list
     return finalScore
 
 main = do
     let ls = getUserInputs
-    printList <- ls
-    print printList
+    final <- partida ls
+    print final
